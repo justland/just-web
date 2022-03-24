3caeea36cdce71cb82c5a1c284581258b94f6d23
Unional <unional@users.noreply.github.com>
2022-03-24 00:19:44 -0700

    chore: show one command

--- just-func/play-react/src/components/CommandPalette.stories.tsx
+++ just-func/play-react/src/components/CommandPalette.stories.tsx
@@ -1,10 +1,18 @@
-import CommandPalette from './CommandPalette'
+import { FC } from 'react'
+import CommandPalette, { CommandPaletteProps } from './CommandPalette'
 
 export default {
   component: CommandPalette
 }
 
-export const NoCommand = () => <>
-<div>ctrl+p to show the command palette</div>
-<CommandPalette commands={[]} />
+const Story: FC<CommandPaletteProps> = ({ commands, ...args }) => <>
+  <div>ctrl+p to show the command palette</div>
+  <CommandPalette commands={commands} {...args} />
 </>
+
+export const NoCommand = () => <Story commands={[]} />
+
+export const OneCommand = () => <Story commands={[{
+  title: 'Simple command',
+  command: 'core.simpleCommand',
+}]} />
