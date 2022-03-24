86f6eccae447eddccfbfabc5d10b28423c8bf64b
Unional <unional@users.noreply.github.com>
2022-03-24 00:48:14 -0700

    test: add visual tests
    Not working yet.
    
    Need to setup react-scripts override to use a different jest config.
    
    Running it all the time is too slow.

--- just-func/play-react/src/setupVisualTests.ts
+++ just-func/play-react/src/setupVisualTests.ts
@@ -1,10 +1,9 @@
 import initStoryshots from '@storybook/addon-storyshots'
-// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
-initStoryshots()
-// initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() })
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() })
