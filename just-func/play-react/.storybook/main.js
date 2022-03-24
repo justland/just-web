4e6dcc5be646b4a612b10844fb29b8da43d55a33
Unional <unional@users.noreply.github.com>
2022-03-24 00:35:54 -0700

    docs: add storysource and chromatic

--- just-func/play-react/.storybook/main.js
+++ just-func/play-react/.storybook/main.js
@@ -1,9 +1,18 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false
+        }
+      }
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-storysource',
     "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
