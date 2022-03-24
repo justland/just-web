53b8821c19658d0cd5e9c4e1737cf8ab4568afd7
Unional <unional@users.noreply.github.com>
2022-03-24 00:19:44 -0700

    chore: show one command

--- just-func/play-react/src/components/CommandPalette.tsx
+++ just-func/play-react/src/components/CommandPalette.tsx
@@ -2,32 +2,31 @@
 import { FC } from 'react'
 import CP from 'react-command-palette'
 import theme from 'react-command-palette/dist/themes/atom-theme'
+import { PartialPick } from 'type-plus'
 import { Command, KeyBinding } from '../commands'
-
 import styles from './CommandPalette.module.css'
 
-function toPaletteCommands(cmds: CommandPalette.Props['commands']) {
+export interface CommandPaletteProps {
+  commands: Array<Command & PartialPick<KeyBinding, 'key'>>
+}
+
+function toPaletteCommands(cmds: CommandPaletteProps['commands']) {
   return cmds.map(c => ({
     name: c.title,
 
-    command: () => alert({ type: 'COMMAND_INVOKE', payload: c.command })
+    command: () => alert(JSON.stringify({ type: 'COMMAND_INVOKE', payload: c.command }))
   }))
 }
 
-const CommandPalette: FC<CommandPalette.Props> = (props) => {
+const CommandPalette: FC<CommandPaletteProps> = (props) => {
   const commands = toPaletteCommands(props.commands)
   return <CP
-    open={true}
     commands={commands}
+    closeOnSelect={true}
+    open={true}
     hotKeys={'ctrl+p'}
     theme={{ ...theme, trigger: styles['command-palette-trigger'] }}
   />
 }
 
 export default CommandPalette
-
-export namespace CommandPalette {
-  export interface Props {
-    commands: Array<Command & KeyBinding>
-  }
-}
