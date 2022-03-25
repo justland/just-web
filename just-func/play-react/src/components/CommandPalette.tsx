8c79c05cac6ce312df937a0f2531f82cfe583fc1
Unional <unional@users.noreply.github.com>
2022-03-25 01:34:22 -0700

    feat: with key

--- just-func/play-react/src/components/CommandPalette.tsx
+++ just-func/play-react/src/components/CommandPalette.tsx
@@ -10,13 +10,20 @@ export interface CommandPaletteProps {
   commands: Array<Command & PartialPick<KeyBinding, 'key'>>
 }
 
+
 function toPaletteCommands(cmds: CommandPaletteProps['commands']) {
   return cmds.map(c => ({
     name: c.title,
-
+    key: c.key,
     command: () => alert(JSON.stringify({ type: 'COMMAND_INVOKE', payload: c.command }))
   }))
 }
+
+const RenderCommand: FC<{ name: string, key?: string }> =
+  command => <div className={styles['suggestion']}>
+    <span className={styles['name']}>{command.name}</span>
+    {command.key && <span className={styles['key']}>{command.key}</span>}
+  </div>
 
 const CommandPalette: FC<CommandPaletteProps> = (props) => {
   const commands = toPaletteCommands(props.commands)
@@ -26,6 +33,7 @@ const CommandPalette: FC<CommandPaletteProps> = (props) => {
     open={true}
     hotKeys={'ctrl+p'}
     theme={{ ...theme, trigger: styles['command-palette-trigger'] }}
+    renderCommand={RenderCommand}
   />
 }
 
