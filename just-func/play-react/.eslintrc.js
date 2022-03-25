51c18e6f2f66bd83dd1d82113bdc493796ee34dd
Unional <unional@users.noreply.github.com>
2022-03-25 03:47:09 -0700

    chore: update lint setting

--- just-func/play-react/.eslintrc.js
+++ just-func/play-react/.eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
-  "extends": ["react-app", "react-app/jest", "plugin:storybook/recommended"],
-  "overrides": [
+  'env': {
+    'es6': true,
+    'jest': true
+  },
+  'extends': [
+    'react-app',
+    'react-app/jest',
+    'plugin:storybook/recommended',
+    'plugin:harmony/ts-recommended'
+  ],
+  'overrides': [
     {
-      "files": [
-        "**/*.stories.*"
+      'files': [
+        '**/*.stories.*'
       ],
-      "rules": {
-        "import/no-anonymous-default-export": "off"
+      'rules': {
+        'import/no-anonymous-default-export': 'off'
       }
     }
   ],
-  "root": true
+  'root': true
 };
--- just-web/platform/.eslintrc.js
+++ just-web/platform/.eslintrc.js
@@ -16,14 +16,6 @@ module.exports = {
         '*.ts',
         '*.tsx'
       ]
-    },
-    {
-      'files': [
-        '**/*.stories.*'
-      ],
-      'rules': {
-        'import/no-anonymous-default-export': 'off'
-      }
     }
   ],
   'root': true
