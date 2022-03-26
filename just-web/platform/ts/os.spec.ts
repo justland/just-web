2425722ef3578e09fb95fa2366c7483d4168ecbe
Unional <unional@users.noreply.github.com>
2022-03-25 21:37:08 -0700

    test: use type-plus stub

--- just-web/platform/ts/os.spec.ts
+++ just-web/platform/ts/os.spec.ts
@@ -1,19 +1,14 @@
-import platform from 'platform'
-import { RecursivePartial } from 'type-plus'
-import { isMacOS } from './os'
+import { stub } from 'type-plus'
+import { isMacOS, Platform } from './os'
 
 describe('isMac()', () => {
   test(`'OS X' is mac`, () => {
-    const platform = stub({ os: { family: 'OS X' } })
+    const platform = stub<Platform>({ os: { family: 'OS X' } })
     expect(isMacOS({ platform })).toEqual(true)
   })
 
   test('`Windows` is not mac', () => {
-    const platform = stub({ os: { family: 'Windows' } })
+    const platform = stub<Platform>({ os: { family: 'Windows' } })
     expect(isMacOS({ platform })).toEqual(false)
   })
 })
-
-function stub(stub: RecursivePartial<typeof platform>) {
-  return stub as typeof platform
-}
