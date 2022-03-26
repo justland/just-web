37669539a283b6c335f0b316950f0345ad3e539d
Unional <unional@users.noreply.github.com>
2022-03-25 23:47:16 -0700

    feat: emit warning for duplicated command

--- just-web/command/ts/commands.spec.ts
+++ just-web/command/ts/commands.spec.ts
@@ -1,6 +1,13 @@
+import { configForTest, MemoryLogReporter } from 'standard-log'
 import { registerCommand } from './commands'
+import { commands } from './store'
+
+let reporter: MemoryLogReporter
+beforeEach(() => reporter = configForTest().reporter)
 
 describe('registerCommand()', () => {
+  beforeEach(() => commands.clear())
+
   test('register a command', () => {
     registerCommand('just-web.commandPalette', {
       description: 'Open Command Palette',
@@ -8,7 +15,9 @@ describe('registerCommand()', () => {
     })
   })
 
-  test.todo('emit warning if registering with existing id', () => {
-
+  test('emit warning if registering with existing id', () => {
+    registerCommand('command1', { description: '', handler() { } })
+    registerCommand('command1', { description: '', handler() { } })
+    expect(reporter.getLogMessageWithLevel()).toEqual(`(WARN) The command 'command1' was already registered`)
   })
 })
