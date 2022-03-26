3372b03a16a2ce4f5582f3d192fbcd981c1f323b
unional <homawong@gmail.com>
2022-03-26 16:42:04 -0700

    chore: use immutable

--- frameworks/command/ts/store.ts
+++ frameworks/command/ts/store.ts
@@ -1,6 +1,3 @@
 import { getLogger } from 'standard-log'
-import { Command } from './types'
-
-export const commands = new Map<string, Command>()
 
 export const log = getLogger('@just-web/command')
