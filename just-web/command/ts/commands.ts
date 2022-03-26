e4c695f7465994ce3cce4d8c3b5cf0fa4e27cef9
Unional <unional@users.noreply.github.com>
2022-03-25 23:47:16 -0700

    feat: emit warning for duplicated command

--- just-web/command/ts/commands.ts
+++ just-web/command/ts/commands.ts
@@ -1,7 +1,11 @@
 import { Omit } from 'type-plus'
-import { commands } from './store'
+import { commands, log } from './store'
 import { Command } from './types'
 
 export function registerCommand(id: string, command: Omit<Command, 'id'>) {
+  if (commands.has(id)) {
+    log.warn(`The command '${id}' was already registered`)
+    return
+  }
   commands.set(id, { ...command, id })
 }
--- just-web/command/ts/store.ts
+++ just-web/command/ts/store.ts
@@ -1,3 +1,6 @@
+import { getLogger } from 'standard-log'
 import { Command } from './types'
 
 export const commands = new Map<string, Command>()
+
+export const log = getLogger('@just-web/command')
