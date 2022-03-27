5cdb14c547f9637b35e94b6cab9ea123fc7688b7
unional <homawong@gmail.com>
2022-03-27 13:19:15 -0700

    feat(play-react): use @just-web/app

--- just-func/play-react/src/index.tsx
+++ just-func/play-react/src/index.tsx
@@ -1,17 +1,30 @@
+import app from '@just-web/app'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+app.routes.registerRoute('/', () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals()
+})
+
+app.routes.registerRoute('/error', () => {
+  ReactDOM.render(
+    <div>something went wrong</div>,
+    document.getElementById('root')
+  )
+})
+
+app.start()
+
