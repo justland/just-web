83d6a697f0fbc010b9f9d11039b2692b5ff0e0f9
unional <homawong@gmail.com>
2022-03-28 02:42:44 -0700

    fix(states): do nto trigger onChagne if value is not changed

--- frameworks/states/ts/state.ts
+++ frameworks/states/ts/state.ts
@@ -11,18 +11,17 @@ export interface OnStateChange<T> {
   (handler: StateChangeHandler<T>): void
 }
 
-export function createState<T>(initValue: T): [T, SetState<T>, OnStateChange<T>] {
+export function createState<T>(value: T): [T, SetState<T>, OnStateChange<T>] {
   const handlers: StateChangeHandler<T>[] = []
-
-  // ? maybe I don't even need this?
-  let value = initValue
 
   return [
     value,
     (newValue: T) => {
+      if (value === newValue) return
+
       const old = value
       value = newValue
-      handlers.forEach(h => h(value, old))
+      handlers.forEach(h => h(newValue, old))
     },
     (handler: StateChangeHandler<T>) => { handlers.push(handler) }
   ]
