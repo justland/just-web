70a0db2aed9cfacf13f3a59d354e1deb14fb1e97
unional <homawong@gmail.com>
2022-03-27 18:10:33 -0700

    chore: add @just-web/states

---
+++ frameworks/states/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
+  roots: [
+    '<rootDir>/ts',
+  ],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+  watchPlugins: [
+    'jest-watch-suspend',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config', { 'setting': 'verbose' },
+    ],
+    [
+      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+    ],
+  ],
+}
