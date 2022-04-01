4540632fd2fd70e16fc639e5e86cd07d1e5cad3e
unional <homawong@gmail.com>
2022-04-01 00:37:28 -0700

    feat(states): add Store interface

--- frameworks/states/ts/store.ts
+++ frameworks/states/ts/store.ts
@@ -1,9 +1,16 @@
-import { createState } from './state'
+import { createState, OnStateChange, ResetState, SetState } from './state'
+
+export interface Store<T> {
+  get(): T,
+  set: SetState<T>,
+  onChange: OnStateChange<T>,
+  reset: ResetState
+}
 
 /**
  * creates a object style store to track a value and its changes.
  */
-export function createStore<T>(value: T) {
+export function createStore<T>(value: T): Store<T> {
   const state = createState(value)
   const [, set, onChange, reset] = state
   onChange(v => state[0] = v)
