3749ff1137bc5d0971998a04681333bee4f562cb
unional <homawong@gmail.com>
2022-04-01 21:53:31 -0700

    refactor(errors): remove errors store from option
    Let the module to handle that itself and returns it.

--- frameworks/errors/ts/module.spec.ts
+++ frameworks/errors/ts/module.spec.ts
@@ -1,13 +1,10 @@
-import { createStore } from '@just-web/states'
-import { ModuleError } from 'iso-error'
+import { start } from './module'
 import * as onerror from './onerror'
-import { start } from './start'
 
 jest.mock('./onerror')
 
 test('by default browser error prevent default is true', async () => {
-  const errors = createStore<ModuleError[]>([])
-  await start({ errors })
+  const { errors } = await start({})
 
   expect(onerror.registerOnErrorHandler).toBeCalledWith({
     errors,
