9354ac13bb2b1f6b8c398c1f4b8c6440aaab2d26
unional <homawong@gmail.com>
2022-04-02 00:22:51 -0700

    feat(states): add buildAdd()

--- frameworks/states/ts/store.spec.ts
+++ frameworks/states/ts/store.spec.ts
@@ -1,4 +1,4 @@
-import { createStore } from './store'
+import { buildAdd, createStore } from './store'
 
 describe('createStore()', () => {
   test('get() returns initial value', () => {
@@ -29,5 +29,18 @@ describe('createStore()', () => {
     const store = createStore(NaN)
     store.onChange(() => { throw 'should not reach' })
     store.set(NaN)
+  })
+})
+
+describe('buildAdd()', () => {
+  test('creates an add function', () => {
+    const store = createStore<Record<string, { key: string, value: number }>>({})
+    const add = buildAdd(store, (record, entry) => record[entry.key] = entry)
+    add({ key: 'a', value: 1 }, { key: 'b', value: 2 })
+
+    expect(store.get()).toEqual({
+      a: { key: 'a', value: 1 },
+      b: { key: 'b', value: 2 }
+    })
   })
 })
