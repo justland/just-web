0d331705689a3a21c7416ab75390d9e9d2022ca3
unional <homawong@gmail.com>
2022-04-02 00:22:51 -0700

    feat(states): add buildAdd()

--- frameworks/states/ts/store.ts
+++ frameworks/states/ts/store.ts
@@ -1,3 +1,5 @@
+import produce, { Draft } from 'immer'
+import { KeyTypes, Pick } from 'type-plus'
 import { createState, OnStateChange, ResetState, SetState } from './state'
 
 export interface Store<T> {
@@ -19,5 +21,20 @@ export function createStore<T>(value: T): Store<T> {
     set,
     onChange,
     reset
+  }
+}
+
+/**
+ * builds an add function for a store
+ */
+export function buildAdd<T, K extends KeyTypes = string | symbol>(
+  store: Pick<Store<Record<K, T>>, 'get' | 'set'>,
+  adder: (record: Draft<Record<K, T>>, entry: T) => void
+) {
+  return function (...entries: T[]) {
+    store.set(produce(
+      store.get(),
+      s => entries.forEach(entry => adder(s, entry))
+    ))
   }
 }
