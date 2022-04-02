134cd99f222f2c2bb4fc669c8c039ed6c10656c3
unional <homawong@gmail.com>
2022-04-01 22:30:30 -0700

    docs(states): add comment

--- frameworks/states/ts/registry.ts
+++ frameworks/states/ts/registry.ts
@@ -1,6 +1,11 @@
 import { KeyTypes, record } from 'type-plus'
 import { createStore } from './store'
 
+/**
+ * @note doing a weird construct to put T before K,
+ * as TypeScript do not yet support optional generic,
+ * and inferring K as string | symbol is a common usage.
+ */
 export function createRegistry<T, K extends KeyTypes = string | symbol>(init?: Record<K, T>) {
   const store = createStore(record<K, T>(init))
   return {
