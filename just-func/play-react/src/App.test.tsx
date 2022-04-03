f2edc021551234e2c6ae9005acc4ca49664642f7
unional <homawong@gmail.com>
2022-04-02 18:35:50 -0700

    test: fix play-react test

--- just-func/play-react/src/App.test.tsx
+++ just-func/play-react/src/App.test.tsx
@@ -1,7 +1,11 @@
 import { render, screen } from '@testing-library/react'
+import { activate } from '@just-web/react-commands'
+import { createContext } from '@just-web/contexts'
 import App from './App'
 
 test('renders learn react link', () => {
+  // This is temporary. the `app` should do this instead
+  activate(createContext())
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
