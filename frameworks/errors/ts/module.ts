2cbad9a2bea9b3e88fef8f000b578c9d2f41f9f8
unional <homawong@gmail.com>
2022-04-04 16:32:00 -0700

    refactor: add module.toReadonly()
    move the ownership of those code to each module.

--- frameworks/errors/ts/module.ts
+++ frameworks/errors/ts/module.ts
@@ -1,5 +1,5 @@
 import * as errorClasses from './errors'
-import { createErrorStore, ErrorStore, ReadonlyErrorStore } from './errorStore'
+import { createErrorStore, ErrorStore, ReadonlyErrorStore, toReadonlyErrorStore } from './errorStore'
 import { registerOnErrorHandler } from './onerror'
 
 export type Module = (typeof errorClasses) & ErrorStore
@@ -19,4 +19,11 @@ export function start(options?: ModuleOptions): Module {
     preventDefault: options?.browserErrors?.preventDefault ?? true
   })
   return Object.assign(errors, errorClasses)
+}
+
+export function toReadonly(module: Module): ReadonlyModule {
+  return {
+    ...module,
+    ...toReadonlyErrorStore(module)
+  }
 }
