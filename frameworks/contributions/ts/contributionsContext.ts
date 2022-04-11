cd96253e3e85757c42cc7fd0f150d54337963d45
unional <homawong@gmail.com>
2022-04-11 01:43:51 -0700

    refactor: update contribution context

--- frameworks/contributions/ts/contributionsContext.ts
+++ frameworks/contributions/ts/contributionsContext.ts
@@ -2,29 +2,29 @@ import { toReadonlyRegistry } from '@just-web/states'
 import { CommandContributionRegistry, commandContributionRegistry, ReadonlyCommandContributionRegistry } from './commands'
 import { KeyBindingContributionRegistry, keyBindingRegistry, ReadonlyKeyBindingContributionRegistry } from './keyBindings'
 
-export interface Module {
+export interface ContributionsContext {
   commands: CommandContributionRegistry,
   keyBindings: KeyBindingContributionRegistry
 }
 
-export interface ReadonlyModule {
+export interface ReadonlyContributionsContext {
   commands: ReadonlyCommandContributionRegistry,
   keyBindings: ReadonlyKeyBindingContributionRegistry
 }
 
-export interface ModuleOptions {
+export interface ContributionsContextOptions {
   commands: commandContributionRegistry.Options['commands'],
   keyBindings: keyBindingRegistry.Options['keyBindings']
 }
 
-export function create(options?: ModuleOptions): Module {
+export function createContributionsContext(options?: ContributionsContextOptions): ContributionsContext {
   const commands = commandContributionRegistry(options)
   const keyBindings = keyBindingRegistry(options)
 
   return { commands, keyBindings }
 }
 
-export function toReadonly(module: Module): ReadonlyModule {
+export function toReadonlyContributionsContext(module: ContributionsContext): ReadonlyContributionsContext {
   return {
     commands: toReadonlyRegistry(module.commands),
     keyBindings: toReadonlyRegistry(module.keyBindings)
