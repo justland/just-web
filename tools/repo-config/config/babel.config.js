06ffd86beb68d25c7765db9875fd4ae820c327d3
unional <homawong@gmail.com>
2022-04-13 00:10:44 -0700

    feat(repo-config): add ts, jest, eslint configs

---
+++ tools/repo-config/config/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    test: {
+      presets: [[
+        '@babel/preset-env',
+        {
+          // debug: true,
+          targets: {
+            node: 'current',
+          },
+        },
+      ], ['@babel/preset-typescript', { allowNamespaces: true }]],
+      plugins: [
+        '@babel/plugin-transform-runtime'
+      ]
+    }
+  }
+}
