d2f0c263838a0e154d8526b9fedb120a32ccd796
unional <homawong@gmail.com>
2022-04-16 01:45:26 -0700

    feat(testing): add logMatchSome()

--- tools/testing/ts/log.spec.ts
+++ tools/testing/ts/log.spec.ts
@@ -1,11 +1,30 @@
-import { createMemoryLogReporter } from '@just-web/log'
+import { configForTest, getLogger, MemoryLogReporter } from '@just-web/log'
 import { isType } from 'type-plus'
-import { assertLog } from './log'
+import { assertLog, logMatchSome } from './log'
+
+let reporter: MemoryLogReporter
+beforeEach(() => reporter = configForTest().reporter)
 
 describe('assertLog()', () => {
   test('require at least one line of expected message', () => {
-    const reporter = createMemoryLogReporter()
     assertLog(reporter, '')
     isType.equal<true, string, Parameters<typeof assertLog>[1]>()
+  })
+})
+
+describe('logMatchSome()', () => {
+  test('match one', () => {
+    const log = getLogger('match one')
+    log.info('first')
+    log.info('second')
+    log.info('third')
+    logMatchSome(reporter, '(INFO) second')
+  })
+  test('match multiple', () => {
+    const log = getLogger('match multi')
+    log.info('first')
+    log.info('second')
+    log.info('third')
+    logMatchSome(reporter, '(INFO) second', '(INFO) third')
   })
 })
