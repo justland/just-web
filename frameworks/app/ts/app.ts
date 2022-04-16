fd032645f19f6fa94e8f36fd2457305849405a9b
unional <homawong@gmail.com>
2022-04-16 00:47:44 -0700

    refactor: absorb `@just-web/plugins` into `@just-web/app`

--- frameworks/app/ts/app.ts
+++ frameworks/app/ts/app.ts
@@ -2,11 +2,10 @@ import '@just-web/commands'
 import { Context, createContext } from '@just-web/contexts'
 import '@just-web/contributions'
 import * as platformModule from '@just-web/platform'
-import * as pluginsModule from '@just-web/plugins'
-import { PluginsContext } from '@just-web/plugins'
 import '@just-web/states'
 import { config, ConfigOptions } from 'standard-log'
 import { log } from './log'
+import { createPluginsContext, PluginsContext, startPlugins } from './plugins/context'
 
 export namespace createApp {
   export interface Options extends createContext.Options {
@@ -24,11 +23,11 @@ export function createApp(options?: createApp.Options): AppContext {
 
   return {
     ...context,
-    ...pluginsModule.createPluginsContext({ context }),
+    ...createPluginsContext({ context }),
     async start() {
       log.notice('application starts')
       await platformModule.start(context)
-      await pluginsModule.start()
+      await startPlugins()
     }
   }
 }
