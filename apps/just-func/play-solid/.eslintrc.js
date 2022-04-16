0a94000770aa9430696a1994a3a32fe7f242f67f
unional <homawong@gmail.com>
2022-04-16 14:51:11 -0700

    feat(repo-scripts): add eslint helper functions

--- apps/just-func/play-solid/.eslintrc.js
+++ apps/just-func/play-solid/.eslintrc.js
@@ -1,44 +1,4 @@
+const { jsx } = require('@just-web/repo-scripts/config/eslint.js')
 const path = require('path')
 
-module.exports = {
-  env: {
-    es6: true,
-    jest: true
-  },
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'plugin:harmony/latest',
-    'plugin:yml/standard',
-  ],
-  overrides: [
-    {
-      extends: [
-        'plugin:harmony/ts-recommended-type-check'
-      ],
-      files: [
-        '*.ts',
-        '*.tsx'
-      ],
-      parserOptions: {
-        project: path.resolve(__dirname, './tsconfig.lint.json')
-      },
-      rules: {
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/require-await': 'off',
-      }
-    },
-    {
-      extends: [
-        'plugin:storybook/recommended'
-      ],
-      files: [
-        '**/*.stories.*'
-      ],
-      rules: {
-        'import/no-anonymous-default-export': 'off'
-      }
-    }
-  ],
-  root: true
-}
+module.exports = jsx(path.resolve(__dirname, 'tsconfig.lint.json'))
---
+++ tools/repo-scripts/config/eslint.js
@@ -0,0 +1,83 @@
+module.exports = {
+  jsx(project) {
+    return {
+      env: {
+        es6: true,
+        jest: true
+      },
+      extends: [
+        'react-app',
+        'react-app/jest',
+        'plugin:harmony/latest',
+        'plugin:yml/standard',
+      ],
+      overrides: [
+        {
+          extends: [
+            'plugin:harmony/ts-recommended-type-check'
+          ],
+          files: [
+            '*.ts',
+            '*.tsx'
+          ],
+          parserOptions: {
+            project,
+            EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true
+          },
+          rules: {
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/require-await': 'off',
+          }
+        },
+        {
+          extends: [
+            'plugin:storybook/recommended'
+          ],
+          files: [
+            '**/*.stories.*'
+          ],
+          rules: {
+            // `userEvent.click(): void` but it is actually returning a promise
+            // https://github.com/storybookjs/eslint-plugin-storybook/issues/45
+            // https://github.com/storybookjs/testing-library/issues/10
+            '@typescript-eslint/await-thenable': 'off',
+            'import/no-anonymous-default-export': 'off'
+          }
+        }
+      ],
+      root: true
+    }
+  },
+  library(project) {
+    return {
+      env: {
+        node: true,
+        es6: true,
+        jest: true
+      },
+      extends: [
+        'plugin:harmony/latest',
+        'plugin:yml/standard'
+      ],
+      overrides: [
+        {
+          extends: [
+            'plugin:harmony/ts-recommended-type-check'
+          ],
+          files: [
+            '*.ts',
+            '*.tsx'
+          ],
+          parserOptions: {
+            project,
+            EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true
+          },
+          rules: {
+            '@typescript-eslint/require-await': 'off'
+          }
+        }
+      ],
+      root: true
+    }
+  }
+}
