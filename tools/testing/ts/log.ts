0b33e8b913ef0a67d6db9c0244705ca714284214
unional <homawong@gmail.com>
2022-04-16 01:45:26 -0700

    feat(testing): add logMatchSome()

--- tools/testing/ts/log.ts
+++ tools/testing/ts/log.ts
@@ -1,6 +1,14 @@
 import { MemoryLogReporter } from '@just-web/log'
+import a from 'assertron'
+import { some } from 'satisfier'
 
 export function assertLog(reporter: MemoryLogReporter, expectedMessage: string, ...expectedMessages: string[]): void
 export function assertLog(reporter: MemoryLogReporter, ...expectedMessages: string[]) {
   expect(reporter.getLogMessageWithLevel()).toEqual(expectedMessages.join('\n'))
+}
+
+export function logMatchSome(reporter: MemoryLogReporter, expectedMessage: string, ...expectedMessages: string[]): void
+export function logMatchSome(reporter: MemoryLogReporter, ...expectedMessages: string[]) {
+  const msgs = reporter.getLogMessageWithLevel().split('\n')
+  expectedMessages.forEach(e => a.satisfies(msgs, some(e)))
 }
