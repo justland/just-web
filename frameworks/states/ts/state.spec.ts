0a864bbe7bd720805328d5f4eb162a8f6b15efec
unional <homawong@gmail.com>
2022-04-18 02:42:00 -0700

    fix(states): onChange register the same handler only once

--- frameworks/states/ts/state.spec.ts
+++ frameworks/states/ts/state.spec.ts
@@ -9,12 +9,12 @@ test('returns initial value', () => {
 test('init value is freezed', () => {
   const init = [1, 2, 3]
   createState(init)
-  expect(()=> init[3] = 4).toThrow()
+  expect(() => init[3] = 4).toThrow()
 })
 
 test('value is freezed', () => {
   const [value] = createState([1, 2, 3])
-  expect(()=> value[3] = 4).toThrow()
+  expect(() => value[3] = 4).toThrow()
 })
 
 test('setValue triggers onChange with new and prev value', () => {
@@ -44,4 +44,15 @@ test('reset() to the original value', () => {
   set(3)
   reset()
   expect(a!).toBe(1)
+})
+
+test('onChange() will register handler only once', () => {
+  const [, set, on] = createState(1)
+  let count = 0
+  const handler = () => count++
+  on(handler)
+  on(handler)
+  set(2)
+
+  expect(count).toBe(1)
 })
