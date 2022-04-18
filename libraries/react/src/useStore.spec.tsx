8971bd0081dc7b90722a6e044af02bb3b67caaed
unional <homawong@gmail.com>
2022-04-18 03:02:47 -0700

    fix(react): useStore onChange should only triggered once

--- libraries/react/src/useStore.spec.tsx
+++ libraries/react/src/useStore.spec.tsx
@@ -1,7 +1,10 @@
-import { createStore } from '@just-web/app'
+import { createStore, createTestApp, logLevels, MemoryLogReporter } from '@just-web/app'
 import { useEffect } from 'react'
 import renderer, { act, ReactTestRendererJSON } from 'react-test-renderer'
 import { useStore } from '.'
+
+let reporter: MemoryLogReporter
+beforeEach(() => reporter = createTestApp({ log: { logLevel: logLevels.all } }).log.reporter)
 
 test('state value updates as store updates', async () => {
   const store = createStore({ counter: 0 })
@@ -74,4 +77,29 @@ test('useEffect to update store', async () => {
   await act(() => tree[1].props.onClick())
 
   expect(store.get().counter).toBe(1)
+})
+
+test('onChange handler called once per change', async () => {
+  const store = createStore({ counter: 0 })
+
+  const Counter = () => {
+    const [value] = useStore(store, s => s.counter)
+
+    return (
+      <>
+        <div>{value}</div>
+      </>
+    )
+  }
+
+  renderer.create(<Counter />)
+  await act(() => store.update(s => { s.counter++ }))
+  await act(() => store.update(s => { s.counter++ }))
+  await act(() => store.update(s => { s.counter++ }))
+  await act(() => store.update(s => { s.counter++ }))
+  await act(() => store.update(s => { s.counter++ }))
+
+  const count = reporter.getLogMessageWithLevel().split('\n')
+    .filter(m => m === `(PLANCK) useStore: onChange triggered`).length
+  expect(count).toBe(5)
 })
