42b5f33adfa62c802ecfb0505ea8b1a33ac00c20
unional <homawong@gmail.com>
2022-04-17 17:59:55 -0700

    feat(app): expose format and some contributions types

--- frameworks/app/ts/index.ts
+++ frameworks/app/ts/index.ts
@@ -1,4 +1,9 @@
+export type {
+  CommandContribution,
+  KeyBindingContribution
+} from '@just-web/contributions'
 export { BrowserError, JustWebError } from '@just-web/errors'
+export * from '@just-web/format'
 export * from '@just-web/log'
 export * from '@just-web/states'
 export * from './app'
