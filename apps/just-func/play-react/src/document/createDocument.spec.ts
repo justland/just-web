74b0f16c2410baa332725037485a217385bc550f
unional <homawong@gmail.com>
2022-04-21 00:02:26 -0700

    feat: add openedFilenames
    replace css module with tailwindcss

--- apps/just-func/play-react/src/document/createDocument.spec.ts
+++ apps/just-func/play-react/src/document/createDocument.spec.ts
@@ -12,7 +12,7 @@ describe('createDocument()', () => {
   test('creates a file named Untitled-1', () => {
     createDocument()
     const s = store.get()
-    const docs = s.documents
+    const docs = s.files
 
     expect(docs.length).toBe(1)
     expect(docs[0].name).toBe('Untitled-1')
@@ -21,28 +21,28 @@ describe('createDocument()', () => {
     createDocument()
     createDocument()
     const s = store.get()
-    const docs = s.documents
+    const docs = s.files
 
     expect(docs.length).toBe(2)
     expect(docs[1].name).toBe('Untitled-2')
   })
   test('create Untitled-x on first non-occupied x', () => {
     store.update(s => {
-      s.documents.push(
+      s.files.push(
         { name: 'Untitled-1', content: '' },
         { name: 'Untitled-3', content: '' }
       )
     })
     createDocument()
     const s = store.get()
-    const d = s.documents[s.documents.length - 1]
+    const d = s.files[s.files.length - 1]
     expect(d.name).toBe('Untitled-2')
   })
 
   test('specify name', () => {
     createDocument('abc')
     const s = store.get()
-    const docs = s.documents
+    const docs = s.files
 
     expect(docs.length).toBe(1)
     expect(docs[0].name).toBe('abc')
