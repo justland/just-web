f9af1b86502422d7b9387dbef0aca75f55a5a746
Homa Wong <homawong@gmail.com>
2022-06-24 01:47:48 -0700

    feat(logs): add `justLog()`

--- frameworks/log/ts/index.ts
+++ frameworks/log/ts/index.ts
@@ -1,3 +1,7 @@
+import { StackTraceMeta } from '@just-func/types'
+import { createStandardLog, Logger, LogMethodNames, LogReporter, ReporterFilter, StandardLogOptions } from 'standard-log'
+import { omit } from 'type-plus'
+
 export {
   createConsoleLogReporter, createMemoryLogReporter, createStandardLogForTest,
   createTimestampFormatter, formatLogLevel, getLogger, InvalidId, logLevels,
@@ -12,3 +16,47 @@ export type {
 } from 'standard-log'
 export * from './context'
 
+export namespace justLog {
+  export interface Param<N extends string = LogMethodNames> extends StandardLogOptions<N> {
+    /**
+     * Name of the application.
+     * When specified, it will be added to the name of the logger,
+     * so that the logs can be associated with the application
+     */
+     name?: string
+  }
+
+  export interface GetLoggerOptions extends StackTraceMeta {
+    id: string,
+    level?: number,
+    writeTo?: LogReporter | ReporterFilter
+  }
+}
+
+export interface JustLog<N extends string = LogMethodNames> {
+  logLevel: number,
+  toLogLevelName(level: number): string,
+  toLogLevel(name: N): number,
+  getLogger(param: justLog.GetLoggerOptions): Logger<N>,
+  error(...args: any[]): void,
+  warn(...args: any[]): void,
+  info(...args: any[]): void,
+  debug(...args: any[]): void,
+}
+
+export function justLog<N extends string = LogMethodNames>(param?: justLog.Param<N>): JustLog<N> {
+  const sl = createStandardLog<N>(param)
+  const name = param?.name
+  const getLogger = name
+    ? function getLogger(param: justLog.GetLoggerOptions) { return sl.getLogger(`${name}:${param.id}`, omit(param, 'id')) }
+    : function getLogger(param: justLog.GetLoggerOptions) { return sl.getLogger(param.id, omit(param, 'id')) }
+  const logger = getLogger({ id: `just-web` })
+  return {
+    ...sl,
+    getLogger,
+    error: logger.error.bind(logger),
+    warn: logger.warn.bind(logger),
+    info: logger.info.bind(logger),
+    debug: logger.debug.bind(logger),
+  }
+}
