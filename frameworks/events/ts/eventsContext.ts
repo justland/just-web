ee410fad454bf769204f575400f0b67c379c2dff
cyberuni <homawong@gmail.com>
2022-09-29 14:48:33 -0700

    chore: add evetns.activate

--- frameworks/events/ts/eventsContext.ts
+++ frameworks/events/ts/eventsContext.ts
@@ -1,4 +1,5 @@
 import { LogContext, Logger } from '@just-web/log'
+import { defineActivate } from '@just-web/types'
 import { EventEmitterLike, trapError } from '@unional/events-plus'
 import { EventEmitter } from 'eventemitter3'
 
@@ -9,6 +10,13 @@ export interface EventsContext {
 export type EventsContextOptions = {
   emitter?: EventEmitterLike
 } & LogContext
+
+export const activate = defineActivate(async (ctx: EventsContextOptions) => {
+  const log = ctx.log.getLogger('@just-web/events')
+  const emitter = trapError(ctx?.emitter || new EventEmitter(), log)
+
+  return [{ emitter }]
+})
 
 export function createEventsContext(options: EventsContextOptions) {
   const log = options.log.getLogger('@just-web/events')
