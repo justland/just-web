fa5a9380b8f831730abb4d10818c2ff541841967
cyberuni <homawong@gmail.com>
2022-09-30 22:24:32 -0700

    refactor: update iso-error

--- frameworks/app/ts/errors.ts
+++ frameworks/app/ts/errors.ts
@@ -1,7 +1,7 @@
-import { IsoError, ModuleError } from 'iso-error'
+import { ModuleError } from 'iso-error'
 
 export class JustWebAppError extends ModuleError {
-  constructor(message: string, options?: IsoError.Options) {
+  constructor(message: string, options?: ModuleError.Options) {
     super('@just-web/app', message, options)
   }
 }
