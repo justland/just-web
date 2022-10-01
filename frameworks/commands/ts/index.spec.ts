47a0043ecf6071f8fabe271ebe2209696a6f7450
cyberuni <homawong@gmail.com>
2022-09-30 19:13:48 -0700

    refactor(commands): export default plugin

--- frameworks/commands/ts/index.spec.ts
+++ frameworks/commands/ts/index.spec.ts
@@ -1,19 +1,19 @@
 import logPlugin from '@just-web/log'
 import contributionsPlugin from '@just-web/contributions'
-import { initialize } from './context'
+import commandsPlugin from '.'
 
 describe('initialize()', () => {
   test('basic case', async () => {
     const [{ log }] = await logPlugin.initForTest()
     const [{ contributions }] = await contributionsPlugin.init({ log })
-    await initialize({ log, contributions })
+    await commandsPlugin.init({ log, contributions })
   })
 
   it('supports predefined commands', async () => {
     const [{ log }] = await logPlugin.initForTest()
     const [{ contributions }] = await contributionsPlugin.init({ log })
     const d = log.getLogger('test')
-    const [{ commands }] = await initialize({
+    const [{ commands }] = await commandsPlugin.init({
       log, contributions, options: {
         commands: {
           'a': () => d.info('exec a'),
--- frameworks/commands/ts/index.ts
+++ frameworks/commands/ts/index.ts
@@ -1,4 +1,52 @@
 
+import { ContributionsContext } from '@just-web/contributions'
+import { LogContext } from '@just-web/log'
+import { definePlugin } from '@just-web/types'
+import { CommandRegistry, commandRegistry } from './commandRegistry'
+
 export * from './commandRegistry'
-export * from './context'
 export type { Command } from './types'
+
+export type CommandsContext = {
+  commands: CommandRegistry
+}
+
+export type CommandsOptions = { commands?: commandRegistry.Options }
+
+export namespace createCommandsContext {
+  export type Context = {
+    options?: CommandsOptions
+  } & LogContext & ContributionsContext
+}
+
+export default definePlugin({
+  init: async (ctx: createCommandsContext.Context): Promise<[CommandsContext]> => {
+    const log = ctx.log.getLogger('@just-web/commands')
+    log.trace('create context')
+    ctx.contributions.commands.add({
+      command: 'just-web.showCommandPalette',
+      commandPalette: false
+    })
+    ctx.contributions.keyBindings.add({
+      command: 'just-web.showCommandPalette',
+      key: 'ctrl+p',
+      mac: 'cmd+p'
+    })
+    return [{ commands: commandRegistry(ctx, ctx.options?.commands) }]
+  }
+})
+
+export function createCommandsContext(ctx: createCommandsContext.Context): CommandsContext {
+  const logger = ctx.log.getLogger('@just-web/commands')
+  logger.trace('create context')
+  ctx.contributions.commands.add({
+    command: 'just-web.showCommandPalette',
+    commandPalette: false
+  })
+  ctx.contributions.keyBindings.add({
+    command: 'just-web.showCommandPalette',
+    key: 'ctrl+p',
+    mac: 'cmd+p'
+  })
+  return { commands: commandRegistry(ctx, ctx.options?.commands) }
+}
