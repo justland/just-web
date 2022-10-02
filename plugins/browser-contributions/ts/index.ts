947f4a32071f39d910324e7e6b5a52aee938e8e0
cyberuni <homawong@gmail.com>
2022-10-02 15:59:07 -0700

    chore: add basic extend and start
    Update TypeScript to 4.8.4, adjusted some types.
    
    There is a bug in TS affecting `definePlugin()`

--- plugins/browser-contributions/ts/index.ts
+++ plugins/browser-contributions/ts/index.ts
@@ -8,7 +8,7 @@ export { BrowserContributionsInitContext, BrowserContributionsStartContext }
 export default definePlugin(() => ({
   name: '@just-web/browser-contributions',
   init: (ctx: BrowserContributionsInitContext) => [undefined, ctx],
-  start: async (ctx: BrowserContributionsStartContext) => {
+  start: async (ctx) => {
     const log = ctx.log.getLogger('@just-web/browser-contributions')
     log.trace('start')
     if (isType<startKeyBindings.Param>(ctx, ctx => !!ctx.contributions)) {
--- plugins/browser-contributions/ts/types.ts
+++ plugins/browser-contributions/ts/types.ts
@@ -2,6 +2,7 @@ import { BrowserContext } from '@just-web/browser'
 import { CommandsContext } from '@just-web/commands'
 import { ContributionsContext } from '@just-web/contributions'
 import { LogContext } from '@just-web/log'
+import { StartContextBase } from '@just-web/types'
 
 export type BrowserContributionsInitContext = LogContext & CommandsContext & ContributionsContext & BrowserContext
-export type BrowserContributionsStartContext = BrowserContributionsInitContext
+export type BrowserContributionsStartContext = StartContextBase & CommandsContext & ContributionsContext & BrowserContext
