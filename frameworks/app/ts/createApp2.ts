5bec879d22bec1d9fc34711ba433b33d0ee2aee8
cyberuni <homawong@gmail.com>
2022-10-02 09:35:11 -0700

    chore(app): add extend()
    Will need more work

--- frameworks/app/ts/createApp2.ts
+++ frameworks/app/ts/createApp2.ts
@@ -1,4 +1,5 @@
 import logPlugin, { LogMethodNames, LogOptions } from '@just-web/log'
+import type { PluginModule } from '@just-web/types'
 import { ctx } from './createApp.ctx'
 
 export namespace createApp2 {
@@ -12,6 +13,15 @@ export function createApp2<N extends string = LogMethodNames>(options: createApp
   return {
     name: appContext.name,
     id: appContext.id,
-    log
+    log,
+    extend<
+      C extends Record<string | symbol, any>,
+      N extends Record<string | symbol, any>,
+      S extends Record<string | symbol, any>>(this: C, plugin: PluginModule<C, N, S, any>): C & N {
+      const initResult = plugin.init(this)
+      if (!initResult) return this as any
+      const [pluginContext] = initResult
+      return { ...this, ...pluginContext! }
+    }
   }
 }
