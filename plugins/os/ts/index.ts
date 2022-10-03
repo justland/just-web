7b6e20879f21b7d47f4d27af6164e1a69faab37b
cyberuni <homawong@gmail.com>
2022-10-03 10:08:16 -0700

    refactor: infers OSContext
    update readme

--- plugins/os/ts/index.ts
+++ plugins/os/ts/index.ts
@@ -1,15 +1,18 @@
 import { LogContext } from '@just-web/log'
 import { definePlugin } from '@just-web/types'
 import { isMac } from './os'
-import type { OSContext } from './types'
 
 export * from './os'
-export { OSContext }
 
-export default definePlugin(() => ({
+const plugin = definePlugin(() => ({
   name: '@just-web/os',
-  init: (ctx: LogContext): [OSContext] => {
+  init: (ctx: LogContext) => {
     ctx.log.notice('init')
+    // this is added to the context so that tests (e.g. storybook) can simulate the behavior in different platforms
     return [{ os: { isMac } }]
   }
 }))
+
+export type OSContext = ReturnType<ReturnType<typeof plugin>['init']>[0]
+
+export default plugin
--- plugins/os/ts/types.ts
+++
@@ -1,7 +0,0 @@
-import { isMac } from './os'
-
-export interface OSContext {
-  os: {
-    isMac: typeof isMac
-  }
-}
