4cdd295ecd93337b7e603a6f2a1dee260531f1a9
cyberuni <homawong@gmail.com>
2022-10-03 14:00:44 -0700

    test: fix test need os plugin

--- apps/just-func/play-react/src/App.test.tsx
+++ apps/just-func/play-react/src/App.test.tsx
@@ -1,12 +1,14 @@
 import { createTestApp } from '@just-web/app'
-import contributionsPlugin from '@just-web/contributions'
 import commandsPlugin from '@just-web/commands'
+import contributionsPlugin from '@just-web/contributions'
+import { osTestPlugin } from '@just-web/os'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import { createAppStore } from './store'
 
 test('renders learn react link', async () => {
   const app = createTestApp()
+    .extend(osTestPlugin())
     .extend(contributionsPlugin())
     .extend(commandsPlugin())
   createAppStore(app as any)
