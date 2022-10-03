f6ecdf53a360c8497d63f02a22931fe48cb22115
cyberuni <homawong@gmail.com>
2022-10-03 09:59:08 -0700

    feat(commands): add showCommandPalette event

--- plugins/commands/ts/index.spec.ts
+++ plugins/commands/ts/index.spec.ts
@@ -1,9 +1,10 @@
 import contributionsPlugin from '@just-web/contributions'
 import { logPluginForTest } from '@just-web/log'
 import { logEqual } from '@just-web/testing'
-import commandsPlugin from '.'
+import commandsPlugin, { showCommandPalette } from '.'
+import { AssertOrder } from 'assertron'
 
-describe('initialize()', () => {
+describe('plugin.init()', () => {
   test('basic case', () => {
     const [{ log }] = logPluginForTest().init()
     const [{ contributions }] = contributionsPlugin().init({ log })
@@ -13,6 +14,7 @@ describe('initialize()', () => {
   it('supports predefined commands', () => {
     const [{ log }] = logPluginForTest().init()
     const [{ contributions }] = contributionsPlugin().init({ log })
+
     const d = log.getLogger('test')
     const [{ commands }] = commandsPlugin({
       commands: {
@@ -20,13 +22,26 @@ describe('initialize()', () => {
         'b': () => d.info('exec b')
       }
     }).init({ log, contributions })
+
     commands.invoke('a')
     commands.invoke('b')
+
     logEqual(log.reporter,
       '(NOTICE) init',
       '(NOTICE) init',
       '(INFO) exec a',
       '(INFO) exec b'
     )
+  })
+
+  it('provides showCommandPalette()', () => {
+    const [{ log }] = logPluginForTest().init()
+    const [{ contributions }] = contributionsPlugin().init({ log })
+    const [{ commands }] = commandsPlugin().init({ log, contributions })
+    const o = new AssertOrder(1)
+    commands.register(showCommandPalette.type, () => o.once(1))
+
+    commands.showCommandPalette()
+    o.end()
   })
 })
