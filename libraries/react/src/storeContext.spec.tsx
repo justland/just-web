4433df2b1f71e318f32ef3407a26c5c38c680abd
cyberuni <homawong@gmail.com>
2022-10-07 12:24:28 -0700

    test(react): update tests

--- libraries/react/src/storeContext.spec.tsx
+++ libraries/react/src/storeContext.spec.tsx
@@ -1,9 +1,18 @@
 import { a } from 'assertron'
+import { create } from 'react-test-renderer'
 import { createStoreContext, useStoreContext } from './storeContext'
 
 describe(`${useStoreContext.name}()`, () => {
   it('throws if Context.Provider is not fill in first', () => {
     const Context = createStoreContext()
-    a.throws(() => useStoreContext(Context, s => s))
+    const Comp = () => {
+      a.throws(
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        () => useStoreContext(Context, s => s),
+        err => err.message === 'Context.Provider must be used before using useStoreContext()')
+
+      return <></>
+    }
+    create(<Comp />)
   })
 })
