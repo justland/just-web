4c5fb2aca62b1e09f93dbc8e451c9d3cde7a91f4
Homa Wong <homawong@gmail.com>
2022-10-26 12:49:33 -0700

    feat(types): add `PluginContext`

--- libraries/react/src/storeContext.tsx
+++ libraries/react/src/storeContext.tsx
@@ -2,10 +2,20 @@ import { Store } from '@just-web/states'
 import { createContext, useContext } from 'react'
 import { useStore } from './useStore'
 
+/**
+ * Creates a `Store<T>`context to be used in `useStoreContext()`
+ * @type T Type of the store value.
+ */
 export function createStoreContext<T>() {
   return createContext<Store<T>>(undefined as any)
 }
 
+/**
+ * Uses a store context.
+ * @param reactContext The context created from `createStoreContext()`.
+ * @param getState The function to get a particular value from the store.
+ * @param updateStore Optional. The function to update the store when the returning `setValue()` is called.
+ */
 export function useStoreContext<S, V>(
   reactContext: React.Context<Store<S>>,
   getState: (s: S) => V,
