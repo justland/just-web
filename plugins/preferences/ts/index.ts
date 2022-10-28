e30cfc92cb8017e8c08c39738e3853dc84d020ea
cyberuni <homawong@gmail.com>
2022-10-27 21:09:20 -0700

    fix(preferences)!: clean up api

--- plugins/preferences/ts/index.ts
+++ plugins/preferences/ts/index.ts
@@ -3,38 +3,36 @@ import { KeyboardContext } from '@just-web/keyboard'
 import { definePlugin } from '@just-web/types'
 
 /**
- * Set the specified user preference.
- * @param key The key of the preference to be update
+ * Gets a specific user preference
+ *
+ * @param key key of the preference. It should be unique across plugins.
+ * This key will be combined with tha app name to form a unique key across host.
+ * So that will work in MFE.
+ * @param defaultValue Optional. The default value to return if the preference doesn't exist.
  */
-export const setUserPreference = command<[key: string, value: string]>('just-web.setUserPreference')
 export const getUserPreference = command<[key: string, defaultValue?: string], string | undefined>('just-web.getUserPreference')
-export const updateUserPreference = command<
-  [key: string, handler: (value: string | undefined) => string]
->('just-web.updateUserPreference')
-
 /**
- * Clear the specified user preference.
- * @param key The key of the preference to be cleared
+ * Set the specified user preference.
+ * @param key The key of the preference to be updated.
+ * @param value new value or value update handler.
+ * If the value or the result of the handler is undefined,
+ * the preference should be removed.
  */
-export const clearUserPreference = command<[key: string]>('just-web.clearUserPreference')
-export const clearUserPreferences = command({ id: 'just-web.clearUserPreferences' })
+export const setUserPreference = command<[key: string, value: string | undefined | ((prevValue: string | undefined) => string | undefined)]>('just-web.setUserPreference')
+export const clearAllUserPreferences = command({ id: 'just-web.clearAllUserPreferences' })
 
 const plugin = definePlugin(() => ({
   name: '@just-web/preferences',
   init: (ctx: CommandsContext & Partial<KeyboardContext>) => {
     getUserPreference.connect(ctx)
     setUserPreference.connect(ctx)
-    updateUserPreference.connect(ctx)
-    clearUserPreference.connect(ctx)
-    clearUserPreferences.connect(ctx)
+    clearAllUserPreferences.connect(ctx)
 
     return [{
       preferences: {
         get: getUserPreference,
         set: setUserPreference,
-        update: updateUserPreference,
-        clear: clearUserPreference,
-        clearAll: clearUserPreferences
+        clearAll: clearAllUserPreferences
       }
     }]
   }
