2dd27d928979fd02bc306857ee4a149ab68efcf2
Homa Wong <homawong@gmail.com>
2022-10-27 19:11:54 -0700

    chore: add useUserPreference
    need to add tests and make it reactive.

---
+++ libraries/react/src/preferences/useUserPreferences.ts
@@ -0,0 +1,40 @@
+import { PreferencesContext } from '@just-web/preferences'
+import { useMemo } from 'react'
+import { JSONTypes } from 'type-plus'
+
+/**
+ * use `@just-web/preference` for a specific value.
+ *
+ * @type T Type of the value. It should be JSON serializable.
+ *
+ * @param app The `just-web` application with `@just-web/preference` (and the specific implementation) loaded.
+ * @param id ID of the preference. This will be the key of the value saved.
+ * It should be unique within the application/plugin.
+ * The current app name will be appended to make it unique across MFE.
+ * @param defaultValue Optional. The default value to return if the preference doesn't exist.
+ *
+ * @return [value, set, update, clear]
+ */
+export function useUserPreference<T extends JSONTypes>(
+  app: PreferencesContext,
+  id: string,
+  defaultValue?: T
+) {
+  const preference = useMemo(() => {
+    const s = app.preferences.get(id)
+    return s ? JSON.parse(s) : defaultValue
+  }, [])
+  function set(state: T) {
+    app.preferences.set(id, JSON.stringify(state))
+  }
+  function update(handler: (old: T) => T) {
+    app.preferences.update(id, (v) => {
+      const old = v ? JSON.parse(v) : defaultValue
+      return JSON.stringify(handler(old))
+    })
+  }
+  function clear() {
+    app.preferences.clear(id)
+  }
+  return [preference, set, update, clear] as const
+}
