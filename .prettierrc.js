77a67ee9e2ac6025409b8876e2aedb39b7cc054e
cyberuni <homawong@gmail.com>
2022-10-28 23:52:17 -0700

    chore: add root level prettier setting
    so that jest snapshot will format the test correctly.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 110,
+  endOfLine: 'auto',
+  semi: false,
+  singleQuote: true,
+
+  // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
+  // to use them for arrays only
+  trailingComma: 'none'
+};
