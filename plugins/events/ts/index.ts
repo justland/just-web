adce4578b82bbd43e5a17c45c2c451db3a0744e3
unional <homawong@gmail.com>
2022-12-11 11:04:27 -0800

    fix: update events-plus
    update repobuddy and test setup.
    Some project should uses jsdom preset

--- plugins/events/ts/index.ts
+++ plugins/events/ts/index.ts
@@ -2,7 +2,8 @@ import type { LogContext } from '@just-web/log'
 import { EventEmitterLike, trapError } from '@unional/events-plus'
 import { EventEmitter } from 'eventemitter3'
 
-export { justEvent, JustEventDuo, JustEventEmpty, JustEventUno } from '@unional/events-plus'
+export { justEvent } from '@unional/events-plus'
+export type { JustEventDuo, JustEventEmpty, JustEventUno } from '@unional/events-plus'
 
 export type EventsOptions<E extends EventEmitterLike> = {
   events?: {
@@ -18,13 +19,14 @@ export type EventsContext<E extends EventEmitterLike> = {
 
 const eventsPlugin = <E extends EventEmitterLike = EventEmitter>(options?: EventsOptions<E>) => ({
   name: '@just-web/events',
-  init: (ctx: LogContext): [EventsContext<E>] => [
-    {
-      events: {
-        emitter: trapError(options?.events?.emitter ?? new EventEmitter(), ctx.log)
+  init: (ctx: LogContext): [EventsContext<E>] =>
+    [
+      {
+        events: {
+          emitter: trapError(options?.events?.emitter ?? new EventEmitter(), ctx.log)
+        }
       }
-    }
-  ] as any
+    ] as any
 })
 
 export default eventsPlugin
