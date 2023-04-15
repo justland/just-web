ab7ddae56f214807ea9e950a3e5232ff4c629d83
unional <homawong@gmail.com>
2023-04-14 22:37:41 -0700

    chore: add docs

--- frameworks/framework/ts/logger.ts
+++ frameworks/framework/ts/logger.ts
@@ -1,6 +1,12 @@
 import { getLogger, type LogMethodNames } from 'standard-log'
 import type { LogGizmo } from './log_gizmo.js'
 
+/**
+ * Creates a function that returns a logger with a prefixed id.
+ *
+ * @param context The log gizmo.
+ * @param prefix The prefix to use.
+ */
 export function createPrefixedGetLogger<N extends string = LogMethodNames>(
 	context: LogGizmo<N>,
 	prefix: string
@@ -10,6 +16,11 @@ export function createPrefixedGetLogger<N extends string = LogMethodNames>(
 	}
 }
 
+/**
+ * Creates a function that returns a non-console logger with a prefixed id.
+ * @param context The log gizmo.
+ * @param prefix The prefix to use.
+ */
 export function createPrefixedGetNonConsoleLogger<N extends string = LogMethodNames>(
 	context: LogGizmo<N>,
 	prefix: string
