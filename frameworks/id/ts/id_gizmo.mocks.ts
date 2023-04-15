9db7278c9a9f6a015facca39cd2125c26f7ded17
unional <homawong@gmail.com>
2023-04-15 14:04:40 -0700

    fix: explicit type declaration
    due to: https://github.com/unional/async-fp/issues/172

--- frameworks/id/ts/id_gizmo.mocks.ts
+++ frameworks/id/ts/id_gizmo.mocks.ts
@@ -1,7 +1,10 @@
-import { define } from '@unional/gizmo'
+import { define, GizmoBase } from '@unional/gizmo'
 import { idGizmoFn, type IdGizmoOptions } from './id_gizmo.js'
 
-export const idTestGizmoFn = define((options: IdGizmoOptions = { name: 'test' }) => ({
+export const idTestGizmoFn: (options?: IdGizmoOptions) => GizmoBase<{
+	id: string
+	name: string
+}> = define((options: IdGizmoOptions = { name: 'test' }) => ({
 	async create() {
 		return idGizmoFn(options).create()
 	}
