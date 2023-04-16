cbab86e3fe14043dfd0c4b12fbef5d459a8fb136
unional <homawong@gmail.com>
2023-04-16 11:17:54 -0700

    refactor: remove extra type declaration

--- plugins/browser/ts/onerror.ts
+++ plugins/browser/ts/onerror.ts
@@ -1,4 +1,4 @@
-import { LogGizmo, Logger, getLogger } from '@just-web/log'
+import { getLogger, type LogGizmo, type Logger } from '@just-web/app'
 import { isType } from 'type-plus'
 import type { ErrorStore } from './error_store.types.js'
 import { BrowserError } from './errors.js'
@@ -9,13 +9,12 @@ export namespace registerOnErrorHandler {
 		errors: ErrorStore
 		preventDefault: boolean
 	}
-	export type Gizmo = LogGizmo
 	export type Meta = { logger: Logger }
 }
 
 export function registerOnErrorHandler(
 	options: registerOnErrorHandler.Options,
-	gizmo?: registerOnErrorHandler.Gizmo
+	gizmo?: LogGizmo
 ): void
 export function registerOnErrorHandler(
 	options: registerOnErrorHandler.Options,
@@ -23,7 +22,7 @@ export function registerOnErrorHandler(
 ): void
 export function registerOnErrorHandler(
 	{ errors, preventDefault }: registerOnErrorHandler.Options,
-	gizmoOrMeta?: registerOnErrorHandler.Gizmo | registerOnErrorHandler.Meta
+	gizmoOrMeta?: LogGizmo | registerOnErrorHandler.Meta
 ): void {
 	const logger = pickLogger(gizmoOrMeta)
 	const window = ctx.getWindow()
@@ -43,8 +42,8 @@ export function registerOnErrorHandler(
 		: justWebOnError
 }
 
-function pickLogger(gizmoOrMeta?: registerOnErrorHandler.Gizmo | registerOnErrorHandler.Meta): Logger {
-	if (isType<registerOnErrorHandler.Gizmo>(gizmoOrMeta, x => x?.log)) {
+function pickLogger(gizmoOrMeta?: LogGizmo | registerOnErrorHandler.Meta): Logger {
+	if (isType<LogGizmo>(gizmoOrMeta, x => x?.log)) {
 		return gizmoOrMeta.log.getLogger('@just-web/browser')
 	}
 	if (isType<registerOnErrorHandler.Meta>(gizmoOrMeta, x => x?.logger)) {
