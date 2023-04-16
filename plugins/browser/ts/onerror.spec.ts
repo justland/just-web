ca1b34db43fc1b21781a4c61b18b16be504be08d
unional <homawong@gmail.com>
2023-04-16 11:26:59 -0700

    test: use justTestApp

--- plugins/browser/ts/onerror.spec.ts
+++ plugins/browser/ts/onerror.spec.ts
@@ -1,8 +1,6 @@
-import { GizmoStandardLog, logLevels } from '@just-web/app'
-import { logLevels } from '@just-web/log'
-import { logTestGizmoFn } from '@just-web/log/testing'
-import { incubate } from '@unional/gizmo'
-import { a } from 'assertron'
+import { logLevels } from '@just-web/app'
+import { justTestApp } from '@just-web/app/testing'
+import { a, some } from 'assertron'
 import { startsWith } from 'satisfier'
 import { createErrorStore } from './error_store.js'
 import { ctx } from './onerror.ctx.js'
@@ -11,7 +9,7 @@ import { registerOnErrorHandler } from './onerror.js'
 it('captures error', async () => {
 	const window = {} as any
 	ctx.getWindow = () => window
-	const { log } = await incubate().with(idTestGizmoFn()).with(logTestGizmoFn()).create()
+	const { log } = await justTestApp().create()
 	const errors = createErrorStore()
 	let actual: Error[]
 	errors.onChange(v => (actual = v))
@@ -33,7 +31,7 @@ it('captures error', async () => {
 it('logs captured error', async () => {
 	const window = {} as any
 	ctx.getWindow = () => window
-	const { log } = await incubate().with(idTestGizmoFn()).with(logTestGizmoFn()).create()
+	const { log } = await justTestApp().create()
 	const errors = createErrorStore()
 
 	registerOnErrorHandler(
@@ -45,24 +43,25 @@ it('logs captured error', async () => {
 	)
 	window.onerror!('some error occurred')
 
-	a.satisfies(log.reporter.logs, [
+	a.satisfies(log.reporter.logs, some(
 		{
 			id: 'test:@just-web/browser',
 			level: logLevels.error,
 			args: startsWith(['onerror detected'])
 		}
-	])
+	))
 })
 
 it('invoke original onerror', async () => {
-	expect.assertions(3)
+	expect.assertions(2)
 
 	const window = { onerror: () => expect(1).toBe(1) } as any
 	ctx.getWindow = () => window
-	const { log } = await incubate().with(idTestGizmoFn()).with(logTestGizmoFn()).create()
+	const { log } = await justTestApp().create()
 	const errors = createErrorStore()
-	let actual: Error[]
-	errors.onChange(v => (actual = v))
+	errors.onChange(v => {
+		expect(v[0].message).toBe('some error occurred')
+	})
 
 	registerOnErrorHandler(
 		{
@@ -73,9 +72,6 @@ it('invoke original onerror', async () => {
 	)
 
 	window.onerror!('some error occurred')
-
-	expect(actual!.length).toBe(1)
-	expect(actual![0].message).toBe('some error occurred')
 })
 
 it('original onerror returns true, result will be true', async () => {
@@ -83,11 +79,10 @@ it('original onerror returns true, result will be true', async () => {
 
 	const window = { onerror: () => true } as any
 	ctx.getWindow = () => window
-	const { log } = await incubate().with(idTestGizmoFn()).with(logTestGizmoFn()).create()
+	const { log } = await justTestApp().create()
 	const errors = createErrorStore()
 	let actual: Error[]
 	errors.onChange(v => (actual = v))
-
 	registerOnErrorHandler(
 		{
 			errors,
