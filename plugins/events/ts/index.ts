82d1fab8d5ca3760ee2f7d2ae7b06ee556023364
unional <homawong@gmail.com>
2023-04-16 12:12:28 -0700

    refactor: move eventsPlugin

--- plugins/events/ts/index.ts
+++ plugins/events/ts/index.ts
@@ -1,32 +1,9 @@
-import type { LogContext } from '@just-web/log'
-import { EventEmitterLike, trapError } from '@unional/events-plus'
-import { EventEmitter } from 'eventemitter3'
+export { eventsPlugin as default, justEvent } from './events_plugin.js'
+export type {
+	EventsContext,
+	EventsOptions,
+	JustEventDuo,
+	JustEventEmpty,
+	JustEventUno
+} from './events_plugin.js'
 
-export { justEvent } from '@unional/events-plus'
-export type { JustEventDuo, JustEventEmpty, JustEventUno } from '@unional/events-plus'
-
-export type EventsOptions<E extends EventEmitterLike> = {
-	events?: {
-		emitter?: E
-	}
-}
-
-export type EventsContext<E extends EventEmitterLike> = {
-	events: {
-		emitter: E
-	}
-}
-
-const eventsPlugin = <E extends EventEmitterLike = EventEmitter>(options?: EventsOptions<E>) => ({
-	name: '@just-web/events',
-	init: (ctx: LogContext): [EventsContext<E>] =>
-		[
-			{
-				events: {
-					emitter: trapError(options?.events?.emitter ?? new EventEmitter(), ctx.log)
-				}
-			}
-		] as any
-})
-
-export default eventsPlugin
