bc9a7206284f033a21152a0311e382cf8790b5dd
unional <homawong@gmail.com>
2023-04-16 19:43:03 -0700

    chore: enable sourceType: module for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,9 @@ module.exports = {
 		node: true,
 		es6: true
 	},
+	parserOptions: {
+		sourceType: 'module'
+	},
 	extends: ['plugin:harmony/latest', 'plugin:yml/standard'],
 	rules: {
 		'yml/quotes': ['error', { prefer: 'single' }]
