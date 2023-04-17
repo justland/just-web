75c1d80495ade12564706c8b130fa608918f3ede
unional <homawong@gmail.com>
2023-04-16 17:44:35 -0700

    fix: add missing export

--- plugins/preferences/ts/preferences_gizmo.mocks.ts
+++ plugins/preferences/ts/preferences_gizmo.mocks.ts
@@ -1,8 +1,7 @@
 import { justTestApp } from '@just-web/app/testing'
 import { commandsGizmoFn } from '@just-web/commands'
 import { keyboardGizmoFn } from '@just-web/keyboard'
-import { memoryPreferencesGizmo } from './index.js'
-import { preferencesGizmo } from './preferences_gizmo.js'
+import { memoryPreferencesGizmo, preferencesGizmo } from './index.js'
 
 export async function setupMemoryPreferencesTestApp() {
 	return justTestApp()
