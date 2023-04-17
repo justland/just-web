3c0da5ccab110273f10509795a8833b86f29f761
unional <homawong@gmail.com>
2023-04-16 17:31:26 -0700

    feat: add browserKeyboardGizmo
    update @unional/gizmo

---
+++ plugins/browser-keyboard/ts/browser_keyboard_gizmo.ts
@@ -0,0 +1,16 @@
+import type { LogGizmo } from '@just-web/app'
+import { define } from '@just-web/app'
+import type { CommandsGizmo } from '@just-web/commands'
+import type { KeyboardGizmo } from '@just-web/keyboard'
+import type { OSGizmo } from '@just-web/os'
+import { startKeyBindings } from './keyBindings.js'
+
+/**
+ * Keyboard Gizmo implementation for the browser.
+ */
+export const browserKeyboardGizmo = define({
+	static: define.require<LogGizmo>().require<KeyboardGizmo>().require<CommandsGizmo>().require<OSGizmo>(),
+	async create(ctx) {
+		return [undefined, () => startKeyBindings(ctx)]
+	}
+})
---
+++ plugins/browser-keyboard/ts/testing/index.ts
@@ -0,0 +1,1 @@
+export * from '../browser_keyboard_gizmos.mock.js'
