f3a90b3e868c98a1ea958d320e767e025c88140c
unional <homawong@gmail.com>
2023-04-19 22:55:55 -0700

    chore: update example

--- examples/browser/ts/index.ts
+++ examples/browser/ts/index.ts
@@ -1,14 +1,14 @@
-import { createTestApp } from '@just-web/app'
-import { logTestPlugin } from '@just-web/log'
-import presetsBrowserPlugin from '@just-web/presets-browser'
-import { createState } from '@just-web/states'
+import { justApp } from '@just-web/app'
+import { commandsGizmoFn } from '@just-web/commands'
+import { presetsBrowserGizmoFn } from '@just-web/presets-browser'
+import { createState, statesGizmo } from '@just-web/states'
 
 export async function main() {
-	const app = await createTestApp({ name: 'browser-app' })
-		.extend(logTestPlugin())
-		.extend(presetsBrowserPlugin())
-
-	await app.start()
+	const app = await justApp({ name: 'browser-app' })
+		.with(commandsGizmoFn())
+		.with(presetsBrowserGizmoFn())
+		.with(statesGizmo)
+		.create()
 
 	const [value] = createState('hello')
 	app.log.info('started', value)
