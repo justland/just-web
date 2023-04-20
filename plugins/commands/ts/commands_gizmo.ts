9e0de21e1432f4ea09c293cc08dadb4aca3110b8
unional <homawong@gmail.com>
2023-04-19 22:12:12 -0700

    chore: update jsdocs and import type

--- plugins/commands/ts/commands_gizmo.ts
+++ plugins/commands/ts/commands_gizmo.ts
@@ -1,4 +1,4 @@
-import { DepBuilder, GizmoStatic, define } from '@just-web/app'
+import { define, type DepBuilder, type GizmoStatic } from '@just-web/app'
 import type { KeyboardGizmo } from '@just-web/keyboard'
 import type { LogGizmo } from '@just-web/log'
 import type { AnyFunction } from 'type-plus'
@@ -12,6 +12,11 @@ export type CommandsGizmoOptions = {
 	handlers?: Record<string, AnyFunction>
 }
 
+/**
+ * Gizmo function to create a CommandsGizmo.
+ *
+ * @require LogGizmo, KeyboardGizmo
+ */
 export const commandsGizmoFn: (options?: CommandsGizmoOptions) => GizmoStatic<
 	DepBuilder<LogGizmo, KeyboardGizmo>,
 	{
@@ -40,4 +45,14 @@ export const commandsGizmoFn: (options?: CommandsGizmoOptions) => GizmoStatic<
 	}
 }))
 
+/**
+ * `CommandsGizmo` is a gizmo that provides the ability to register commands.
+ *
+ * Each command has two parts:
+ * - a contribution, which is a static object that describes the command.
+ * - a handler, which is a function that is called when the command is executed.
+ *
+ * This means the command contribution can be defined by one gizmo,
+ * and the implementation of the command can be provided by another gizmo.
+ */
 export type CommandsGizmo = define.Infer<typeof commandsGizmoFn>
