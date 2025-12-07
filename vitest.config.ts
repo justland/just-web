70c8fa856a62b76e65c8e00c4c39219b9cc3eb83
unional <homawong@gmail.com>
2025-12-07 13:20:38 -0800

    chore(deps): migrate coverage to vitest and update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		coverage: {
-			include: ['ts/**/*.[jt]s'],
-			exclude: ['ts/bin.[jt]s']
-		}
+			include: ['src/**/*.[jt]s'],
+			exclude: ['src/bin.[jt]s']
+		},
+		projects: ['components/*', 'frameworks/*', 'libraries/*', 'plugins/*', 'presets/*', 'tools/*']
 	}
 })
