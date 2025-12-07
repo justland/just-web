0052a22073ece953e987b9570688e6cc2ec1836a
unional <homawong@gmail.com>
2025-12-07 13:36:36 -0800

    chore(deps): remove cross-env

--- knip.config.ts
+++ knip.config.ts
@@ -1,8 +1,13 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
+	vitest: {
+		entry: ['**/*.{spec,test,unit,accept,integrate,system,perf,stress}.*']
+	},
 	ignore: ['**/tslib/*', '*/*/testing/*'],
 	ignoreDependencies: [
+		// Need patching
+		'@changesets/assemble-release-plan',
 		// Build and tooling dependencies
 		'@repobuddy/*',
 		'@size-limit/*',
