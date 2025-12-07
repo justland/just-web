d5e24d2badc1f05d59d5731640b404b5ef6ceb7d
unional <homawong@gmail.com>
2025-12-07 11:23:45 -0800

    refactor: update browser vitest config to use preset

--- plugins/browser/vitest.config.ts
+++ plugins/browser/vitest.config.ts
@@ -1,8 +1,9 @@
+import { browserTestPreset } from '@repobuddy/vitest/config/browser'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+	plugins: [browserTestPreset({ includeGeneralTests: true })],
 	test: {
-		name: 'browser',
-		environment: 'jsdom'
+		name: 'browser'
 	}
 })
